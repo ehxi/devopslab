22dc5e33089d30e1362efaa17b9e6f4c81dd3ee3
ehxi <115420594+ehxi@users.noreply.github.com>
2023-03-04 13:37:42 -0300

    Corrigindo bug 2

--- app.py
+++ app.py
@@ -2,13 +2,8 @@ from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
-
 csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
     return "Olá, Sou o Ehxi! ( leia-se Ash! )"
-
-if __name__ == '__main__':
-    app.run()
-    
